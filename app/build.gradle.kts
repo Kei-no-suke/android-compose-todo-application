7be1c8bb2f952a2b4bece7cec1b85b0a9f635abc
Kei-no-suke <55536596+Kei-no-suke@users.noreply.github.com>
2025-02-22 16:33:17 +0900

    update library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "2.1.10-1.0.30"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20-RC"
 }
 
 android {
     namespace = "com.keinosuke.todoapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.keinosuke.todoapplication"
